20d1ae84443bc05f4f15034b858dc71921c2ef2b
Vincent Villaluna <villalunavincent@gmail.com>
2019-07-02 17:06:00 +0800

    added data_types

---
+++ data_types/main.go
@@ -0,0 +1,34 @@
+package main
+
+func main() {
+	//uint8 has a max value of 0 - 255
+	var uinteight uint8 = 255
+
+	//uint16 has a max value of 0 - 65535
+	var uintsixteen uint16 = 65535
+
+	//uint32 has a max value of 0 - 4294967295
+	var uintthirtytwo uint32 = 4294967295
+
+	//uint64 has a max value of 0 - 18446744073709551615
+	var uintsixtyfour uint64 = 18446744073709551615
+
+	//int8 has 8 bit integers -128 - 127
+	var inteight int8 = 127
+	inteight = -128
+
+	//int16 has 16 bit integers -32768 - 32767
+	var intsixteen int16 = 32767
+	intsixteen = -32768
+
+	//int32 has 32 bit integers -2147483648 - 2147483647
+	var intthirtytwo int32 = 2147483647
+	intthirtytwo = -2147483648
+
+	//int64 has 64 bit integers -9223372036854775808 to 9223372036854775807
+	var intsixtyfour int64 = 9223372036854775807
+	intsixtyfour = -9223372036854775808
+
+	//some of the data types here:
+	// https://www.tutorialspoint.com/go/go_data_types.htm
+}
